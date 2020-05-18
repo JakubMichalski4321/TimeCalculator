f6c05fb3fdee3abfc8b5c0ebf73538ece0750b9e
Jackob <Jackob@DESKTOP-S5MFNHV>
2020-05-18 22:35:50 +0200

    czas garbacki

--- app/src/main/java/com/example/club27/MainActivity.java
+++ app/src/main/java/com/example/club27/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.club27;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button obliczButton = (Button) findViewById(R.id.obliczButton);
+        obliczButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
