72bb761d52c5cfd82d2f0a17da3013a4837c5927
Jackob <Jackob@DESKTOP-S5MFNHV>
2020-05-18 23:29:15 +0200

    czas garbacki

--- app/src/main/java/com/example/club27/MainActivity.java
+++ app/src/main/java/com/example/club27/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.club27;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
         obliczButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText minutesSetEditText = (EditText) findViewById(R.id.minutesSetEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultText);
+
+                double minutes = Integer.parseInt(minutesSetEditText.getText().toString());
 
             }
         });
